91c9cd36d1e554bc9660d21be4e8750897c1ecdb
Nicholas Vrtis <nvav91@gmail.com>
2020-12-10 17:24:45 +0100

    updated color changing

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
+// Const and variable list
 const nineInput = $('#user-input9')
+const rowValueNine = $('#time1').text()
 const tenInput = $('#user-input10')
+const rowValueTen = $('#time2').text()
 const elevenInput =  $('#user-input11')
+const rowValueEleven = $('#time3').text()
 const twelveInput =  $('#user-input12')
+const rowValueTwelve = $('#time4').text()
 const thirteenInput=  $('#user-input13')
+const rowValueThirteen = $('#time5').text()
 const fourteenInput = $('#user-input14')
+const rowValueFourteen = $('#time6').text()
 const fifteenInput =  $('#user-input15')
+const rowValueFifteen = $('#time7').text()
 const sixsteenInput =  $('#user-input16')
+const rowValueSixsteen = $('#time8').text()
 const seventeenInput =  $('#user-input17')
+const rowValueSeventeen = $('#time9').text()
 const input =  $('.input')
 const timeBlock =  $('.time-block')
+const row =  $('.row')
 
 const saveBtn =  $('.btn')
 const todaysDateDisplay = $('#currentDay')
@@ -22,15 +33,20 @@ function showTime () {
     todaysDateDisplay.html(todaysDate)
     updateTime()
 }
-// function for the timer to update everysecond
+// function for the timer to update every second
 function updateTime () {
     var update=1000
     setTimeout(showTime, update)
 }
+// calling function for loading it with the page
     showTime()
+// eventlistener for getting users input for task
+    saveBtn.on("click", getTask)
     
-    saveBtn.on("click", function (event) {
+// get task function
+    function getTask(event) {
         event.preventDefault();
+        // save task as a variable
         var nineValue = nineInput.val();
         var tenValue = tenInput.val();
         var elevenValue = elevenInput.val();
@@ -41,7 +57,7 @@ function updateTime () {
         var sixsteenValue = sixsteenInput.val();
         var seventeenValue = seventeenInput.val();
     
-    
+    // setting task in the local storage
         localStorage.setItem("9OClock", JSON.stringify(nineValue));
         localStorage.setItem("10OClock", JSON.stringify(tenValue));
         localStorage.setItem("11OClock", JSON.stringify(elevenValue));
@@ -51,11 +67,13 @@ function updateTime () {
         localStorage.setItem("15OClock", JSON.stringify(fifteenValue));
         localStorage.setItem("16OClock", JSON.stringify(sixsteenValue));
         localStorage.setItem("17OClock", JSON.stringify(seventeenValue));
-    });
+    };
     
+    // calling displayText when page loads
     displayText();
-    
+    // displayText function for recieving from local storage and displaying in input field
     function displayText() {
+        // taking from local storage and saving as a variable
         var nine = JSON.parse(localStorage.getItem("9OClock",)) || [];
         var ten = JSON.parse(localStorage.getItem("10OClock",)) || [];
         var eleven = JSON.parse(localStorage.getItem("11OClock",)) || [];
@@ -66,6 +84,7 @@ function updateTime () {
         var sixsteen = JSON.parse(localStorage.getItem("16OClock",)) || [];
         var seventeen = JSON.parse(localStorage.getItem("17OClock",)) || [];
     
+        // putting the variable into the input field
         nineInput.val(nine)
         tenInput.val(ten)
         elevenInput.val(eleven)
@@ -78,235 +97,170 @@ function updateTime () {
     
     }
 
-    if(todaysHour===17) {
-        saveBtn.removeClass('btn-primary')
-        saveBtn.removeClass('btn-success')
-        saveBtn.removeClass('btn-secondary')
-        saveBtn.addClass('btn-secondary')
-        $('#btn17').removeClass('btn-secondary')
-        $('#btn17').addClass('btn-primary')
-        input.css("background-color", "grey")
-        seventeenInput.css("background-color", "white")
-        timeBlock.css("background-color", "darkgrey")
-        $('#time-block-9').css("background-color", "green")
+    if (todaysHour == rowValueNine){
+        $('#row1').removeClass('past')
+        $('#row1').removeClass('present')
+        $('#row1').removeClass('future')
+        $('#row1').addClass('present')
     }
-    else if(todaysHour===16) {
-        saveBtn.removeClass('btn-primary')
-        saveBtn.removeClass('btn-success')
-        saveBtn.removeClass('btn-secondary')
-        saveBtn.addClass('btn-secondary')
-        $('#btn17').removeClass('btn-secondary')
-        $('#btn16').removeClass('btn-secondary')
-        $('#btn17').addClass('btn-success')
-        $('#btn16').addClass('btn-primary')
-        input.css("background-color", "grey")
-        seventeenInput.css("background-color", "white")
-        sixsteenInput.css("background-color", "white")
-        timeBlock.css("background-color", "darkgrey")
-        $('#time-block-9').css("background-color", "lightgreen")
-        $('#time-block-8').css("background-color", "green")
-    }
-    else if(todaysHour===15) {
-        saveBtn.removeClass('btn-primary')
-        saveBtn.removeClass('btn-success')
-        saveBtn.removeClass('btn-secondary')
-        saveBtn.addClass('btn-secondary')
-        $('#btn17').removeClass('btn-secondary')
-        $('#btn16').removeClass('btn-secondary')
-        $('#btn15').removeClass('btn-secondary')
-        $('#btn17').addClass('btn-success')
-        $('#btn16').addClass('btn-success')
-        $('#btn15').addClass('btn-primary')
-        input.css("background-color", "grey")
-        seventeenInput.css("background-color", "white")
-        sixsteenInput.css("background-color", "white")
-        fifteenInput.css("background-color", "white")
-        timeBlock.css("background-color", "darkgrey")
-        $('#time-block-9').css("background-color", "lightgreen")
-        $('#time-block-8').css("background-color", "lightgreen")
-        $('#time-block-7').css("background-color", "green")
-    }
-    else if(todaysHour===14) {
-        saveBtn.removeClass('btn-primary')
-        saveBtn.removeClass('btn-success')
-        saveBtn.removeClass('btn-secondary')
-        saveBtn.addClass('btn-secondary')
-        $('#btn17').removeClass('btn-secondary')
-        $('#btn16').removeClass('btn-secondary')
-        $('#btn15').removeClass('btn-secondary')
-        $('#btn14').removeClass('btn-secondary')
-        $('#btn17').addClass('btn-success')
-        $('#btn16').addClass('btn-success')
-        $('#btn15').addClass('btn-success')
-        $('#btn14').addClass('btn-primary')
-        input.css("background-color", "grey")
-        seventeenInput.css("background-color", "white")
-        sixsteenInput.css("background-color", "white")
-        fifteenInput.css("background-color", "white")
-        fourteenInput.css("background-color", "white")
-        timeBlock.css("background-color", "darkgrey")
-        $('#time-block-9').css("background-color", "lightgreen")
-        $('#time-block-8').css("background-color", "lightgreen")
-        $('#time-block-7').css("background-color", "lightgreen")
-        $('#time-block-6').css("background-color", "green")
-    }
-
-    else if(todaysHour===13) {
-        saveBtn.removeClass('btn-primary')
-        saveBtn.removeClass('btn-success')
-        saveBtn.removeClass('btn-secondary')
-        saveBtn.addClass('btn-secondary')
-        $('#btn17').removeClass('btn-secondary')
-        $('#btn16').removeClass('btn-secondary')
-        $('#btn15').removeClass('btn-secondary')
-        $('#btn14').removeClass('btn-secondary')
-        $('#btn13').removeClass('btn-secondary')
-        $('#btn17').addClass('btn-success')
-        $('#btn16').addClass('btn-success')
-        $('#btn15').addClass('btn-success')
-        $('#btn14').addClass('btn-success')
-        $('#btn13').addClass('btn-primary')
-        input.css("background-color", "grey")
-        seventeenInput.css("background-color", "white")
-        sixsteenInput.css("background-color", "white")
-        fifteenInput.css("background-color", "white")
-        fourteenInput.css("background-color", "white")
-        thirteenInput.css("background-color", "white")
-        timeBlock.css("background-color", "darkgrey")
-        $('#time-block-9').css("background-color", "lightgreen")
-        $('#time-block-8').css("background-color", "lightgreen")
-        $('#time-block-7').css("background-color", "lightgreen")
-        $('#time-block-6').css("background-color", "lightgreen")
-        $('#time-block-5').css("background-color", "green")
-    }
-
-    else if(todaysHour===12) {
-        saveBtn.removeClass('btn-primary')
-        saveBtn.removeClass('btn-success')
-        saveBtn.removeClass('btn-secondary')
-        saveBtn.addClass('btn-secondary')
-        $('#btn17').removeClass('btn-secondary')
-        $('#btn16').removeClass('btn-secondary')
-        $('#btn15').removeClass('btn-secondary')
-        $('#btn14').removeClass('btn-secondary')
-        $('#btn13').removeClass('btn-secondary')
-        $('#btn12').removeClass('btn-secondary')
-        $('#btn17').addClass('btn-success')
-        $('#btn16').addClass('btn-success')
-        $('#btn15').addClass('btn-success')
-        $('#btn14').addClass('btn-success')
-        $('#btn13').addClass('btn-success')
-        $('#btn12').addClass('btn-primary')
-        input.css("background-color", "grey")
-        seventeenInput.css("background-color", "white")
-        sixsteenInput.css("background-color", "white")
-        fifteenInput.css("background-color", "white")
-        fourteenInput.css("background-color", "white")
-        thirteenInput.css("background-color", "white")
-        twelveInput.css("background-color", "white")
-        timeBlock.css("background-color", "darkgrey")
-        $('#time-block-9').css("background-color", "lightgreen")
-        $('#time-block-8').css("background-color", "lightgreen")
-        $('#time-block-7').css("background-color", "lightgreen")
-        $('#time-block-6').css("background-color", "lightgreen")
-        $('#time-block-5').css("background-color", "lightgreen")
-        $('#time-block-4').css("background-color", "green")
-    }
-
-    else if(todaysHour===11) {
-        saveBtn.removeClass('btn-primary')
-        saveBtn.removeClass('btn-success')
-        saveBtn.removeClass('btn-secondary')
-        saveBtn.addClass('btn-secondary')
-        $('#btn17').removeClass('btn-secondary')
-        $('#btn16').removeClass('btn-secondary')
-        $('#btn15').removeClass('btn-secondary')
-        $('#btn14').removeClass('btn-secondary')
-        $('#btn13').removeClass('btn-secondary')
-        $('#btn12').removeClass('btn-secondary')
-        $('#btn11').removeClass('btn-secondary')
-        $('#btn17').addClass('btn-success')
-        $('#btn16').addClass('btn-success')
-        $('#btn15').addClass('btn-success')
-        $('#btn14').addClass('btn-success')
-        $('#btn13').addClass('btn-success')
-        $('#btn12').addClass('btn-success')
-        $('#btn11').addClass('btn-primary')
-        input.css("background-color", "grey")
-        seventeenInput.css("background-color", "white")
-        sixsteenInput.css("background-color", "white")
-        fifteenInput.css("background-color", "white")
-        fourteenInput.css("background-color", "white")
-        thirteenInput.css("background-color", "white")
-        twelveInput.css("background-color", "white")
-        elevenInput.css("background-color", "white")
-        timeBlock.css("background-color", "darkgrey")
-        $('#time-block-9').css("background-color", "lightgreen")
-        $('#time-block-8').css("background-color", "lightgreen")
-        $('#time-block-7').css("background-color", "lightgreen")
-        $('#time-block-6').css("background-color", "lightgreen")
-        $('#time-block-5').css("background-color", "lightgreen")
-        $('#time-block-4').css("background-color", "lightgreen")
-        $('#time-block-3').css("background-color", "green")
-    }
-
-    else if(todaysHour===10) {
-        saveBtn.removeClass('btn-primary')
-        saveBtn.removeClass('btn-success')
-        saveBtn.removeClass('btn-secondary')
-        saveBtn.addClass('btn-secondary')
-        $('#btn17').removeClass('btn-secondary')
-        $('#btn16').removeClass('btn-secondary')
-        $('#btn15').removeClass('btn-secondary')
-        $('#btn14').removeClass('btn-secondary')
-        $('#btn13').removeClass('btn-secondary')
-        $('#btn12').removeClass('btn-secondary')
-        $('#btn11').removeClass('btn-secondary')
-        $('#btn10').removeClass('btn-secondary')
-        $('#btn17').addClass('btn-success')
-        $('#btn16').addClass('btn-success')
-        $('#btn15').addClass('btn-success')
-        $('#btn14').addClass('btn-success')
-        $('#btn13').addClass('btn-success')
-        $('#btn12').addClass('btn-success')
-        $('#btn11').addClass('btn-success')
-        $('#btn10').addClass('btn-primary')
-        input.css("background-color", "grey")
-        seventeenInput.css("background-color", "white")
-        sixsteenInput.css("background-color", "white")
-        fifteenInput.css("background-color", "white")
-        fourteenInput.css("background-color", "white")
-        thirteenInput.css("background-color", "white")
-        twelveInput.css("background-color", "white")
-        elevenInput.css("background-color", "white")
-        tenInput.css("background-color", "white")
-        timeBlock.css("background-color", "darkgrey")
-        $('#time-block-9').css("background-color", "lightgreen")
-        $('#time-block-8').css("background-color", "lightgreen")
-        $('#time-block-7').css("background-color", "lightgreen")
-        $('#time-block-6').css("background-color", "lightgreen")
-        $('#time-block-5').css("background-color", "lightgreen")
-        $('#time-block-4').css("background-color", "lightgreen")
-        $('#time-block-3').css("background-color", "lightgreen")
-        $('#time-block-2').css("background-color", "green")
-    }
-    else if(todaysHour<=9) {
-        saveBtn.removeClass('btn-primary')
-        saveBtn.removeClass('btn-success')
-        saveBtn.removeClass('btn-secondary')
-        saveBtn.addClass('btn-success')
-        $('#btn9').removeClass('btn-success')
-        $('#btn9').addClass('btn-primary')
-        input.css("background-color", "white")
-        timeBlock.css("background-color", "lightgreen")
-        $('#time-block-1').css("background-color", "green")
-    }
-
+    else if (todaysHour > rowValueNine) {
+        $('#row1').removeClass('past')
+        $('#row1').removeClass('present')
+        $('#row1').removeClass('future')
+        $('#row1').addClass('past')
+}
     else {
-        saveBtn.removeClass('btn-primary')
-        saveBtn.removeClass('btn-success')
-        saveBtn.removeClass('btn-secondary')
-        saveBtn.addClass('btn-secondary')
-        input.css("background-color", "grey")
-        timeBlock.css("background-color", "darkgrey")
+        $('#row1').removeClass('past')
+        $('#row1').removeClass('present')
+        $('#row1').removeClass('future')
+        $('#row1').addClass('future')
+    }
+
+    if (todaysHour == rowValueTen){
+        $('#row2').removeClass('past')
+        $('#row2').removeClass('present')
+        $('#row2').removeClass('future')
+        $('#row2').addClass('present')
+    }
+    else if (todaysHour > rowValueTen) {
+        $('#row2').removeClass('past')
+        $('#row2').removeClass('present')
+        $('#row2').removeClass('future')
+        $('#row2').addClass('past')
+}
+    else {
+        $('#row2').removeClass('past')
+        $('#row2').removeClass('present')
+        $('#row2').removeClass('future')
+        $('#row2').addClass('future')
+    }
+
+    if (todaysHour == rowValueEleven){
+        $('#row3').removeClass('past')
+        $('#row3').removeClass('present')
+        $('#row3').removeClass('future')
+        $('#row3').addClass('present')
+    }
+    else if (todaysHour > rowValueEleven) {
+        $('#row3').removeClass('past')
+        $('#row3').removeClass('present')
+        $('#row3').removeClass('future')
+        $('#row3').addClass('past')
+}
+    else {
+        $('#row3').removeClass('past')
+        $('#row3').removeClass('present')
+        $('#row3').removeClass('future')
+        $('#row3').addClass('future')
+    }
+    if (todaysHour == rowValueTwelve){
+        $('#row4').removeClass('past')
+        $('#row4').removeClass('present')
+        $('#row4').removeClass('future')
+        $('#row4').addClass('present')
+    }
+    else if (todaysHour > rowValueTwelve) {
+        $('#row4').removeClass('past')
+        $('#row4').removeClass('present')
+        $('#row4').removeClass('future')
+        $('#row4').addClass('past')
+}
+    else {
+        $('#row4').removeClass('past')
+        $('#row4').removeClass('present')
+        $('#row4').removeClass('future')
+        $('#row4').addClass('future')
+    }
+
+    if (todaysHour == rowValueThirteen){
+        $('#row5').removeClass('past')
+        $('#row5').removeClass('present')
+        $('#row5').removeClass('future')
+        $('#row5').addClass('present')
+    }
+    else if (todaysHour > rowValueThirteen) {
+        $('#row5').removeClass('past')
+        $('#row5').removeClass('present')
+        $('#row5').removeClass('future')
+        $('#row5').addClass('past')
+}
+    else {
+        $('#row5').removeClass('past')
+        $('#row5').removeClass('present')
+        $('#row5').removeClass('future')
+        $('#row5').addClass('future')
+    }
+    if (todaysHour == rowValueFourteen){
+        $('#row6').removeClass('past')
+        $('#row6').removeClass('present')
+        $('#row6').removeClass('future')
+        $('#row6').addClass('present')
+    }
+    else if (todaysHour > rowValueFourteen) {
+        $('#row6').removeClass('past')
+        $('#row6').removeClass('present')
+        $('#row6').removeClass('future')
+        $('#row6').addClass('past')
+}
+    else {
+        $('#row6').removeClass('past')
+        $('#row6').removeClass('present')
+        $('#row6').removeClass('future')
+        $('#row6').addClass('future')
+    }
+
+    if (todaysHour == rowValueFifteen){
+        $('#row7').removeClass('past')
+        $('#row7').removeClass('present')
+        $('#row7').removeClass('future')
+        $('#row7').addClass('present')
+    }
+    else if (todaysHour > rowValueFifteen) {
+        $('#row7').removeClass('past')
+        $('#row7').removeClass('present')
+        $('#row7').removeClass('future')
+        $('#row7').addClass('past')
+}
+    else {
+        $('#row7').removeClass('past')
+        $('#row7').removeClass('present')
+        $('#row7').removeClass('future')
+        $('#row7').addClass('future')
+    }
+
+    if (todaysHour == rowValueSixsteen){
+        $('#row8').removeClass('past')
+        $('#row8').removeClass('present')
+        $('#row8').removeClass('future')
+        $('#row8').addClass('present')
+    }
+    else if (todaysHour > rowValueSixsteen) {
+        $('#row8').removeClass('past')
+        $('#row8').removeClass('present')
+        $('#row8').removeClass('future')
+        $('#row8').addClass('past')
+}
+    else {
+        $('#row8').removeClass('past')
+        $('#row8').removeClass('present')
+        $('#row8').removeClass('future')
+        $('#row8').addClass('future')
+    }
+    if (todaysHour == rowValueSeventeen){
+        $('#row9').removeClass('past')
+        $('#row9').removeClass('present')
+        $('#row9').removeClass('future')
+        $('#row9').addClass('present')
+    }
+    else if (todaysHour > rowValueSeventeen) {
+        $('#row9').removeClass('past')
+        $('#row9').removeClass('present')
+        $('#row9').removeClass('future')
+        $('#row9').addClass('past')
+}
+    else {
+        $('#row9').removeClass('past')
+        $('#row9').removeClass('present')
+        $('#row9').removeClass('future')
+        $('#row9').addClass('future')
     }
